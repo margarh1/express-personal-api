3fa84eb1f801c42afc54f662dd48d7c00083a39a
margarh1 <ha.margaret1@gmail.com>
2016-11-13 18:43:54 -0800

    added ability to reset input, still failing to get deletion to work

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,7 +1,7 @@
 console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
-
+  
   $.ajax({
     method: 'GET',
     url: '/api/vacations',
@@ -10,18 +10,52 @@ $(document).ready(function(){
     compelte: console.log('Finished')
   });
 
-  function onSuccess(json) {
-    var vacationsSource = $('#vacations-template').html();
-    var vacationsTemplate = Handlebars.compile(vacationsSource);
-    var vacationsHtml = vacationsTemplate({ vacations: json });
-    $('.vacations').append(vacationsHtml);
-  };
+  $('#newVacationForm').on('submit', function(event) {
+    event.preventDefault();
+    $.ajax({
+      method: 'POST',
+      url: '/api/vacations',
+      data: $(this).serialize(),
+      success: addNewVacation,
+      error: onError,
+      complete: console.log('New Vacation')
+    })
+  });
 
-  function onError(xhr, status, errorThrown) {
-    alert('Sorry, there was a problem!');
-    console.log('Error: ' + errorThrown);
-    console.log('Status: ' + status);
-    console.dir(xhr);
-  }
+  $('.delete-button').on('click', function() {
+    console.log('this');
+    // $.ajax({
+    //   method: 'DELETE',
+    //   url: 'api/vacations/' + $(this).attr('data-id'),
+    //   success: deleteVacationSuccess,
+    //   error: onError,
+    //   complete: console.log('Deleted Vacation')
+    // })
+  })
 
 });
+
+function onSuccess(json) {
+  var vacationsSource = $('#vacations-template').html();
+  var vacationsTemplate = Handlebars.compile(vacationsSource);
+  var vacationsHtml = vacationsTemplate({ vacations: json });
+  $('.vacations').append(vacationsHtml);
+};
+
+function onError(xhr, status, errorThrown) {
+  alert('Sorry, there was a problem!');
+  console.log('Error: ' + errorThrown);
+  console.log('Status: ' + status);
+  console.dir(xhr);
+};
+
+function addNewVacation(json) {
+  $('#newVacationForm input').val('');
+  $('input:checked').prop('checked', false);
+  console.log(json.location);
+};
+
+function deleteVacationSucces(json) {
+  console.log(json);
+};
+
